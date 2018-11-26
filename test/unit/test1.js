41066808d9eaa6630487aaceff35435f17f5c9f5
David Gleghorne <david.gleghorne@gmail.com>
2018-11-26 13:15:40 +0000

    add test file

---
+++ test/unit/test1.js
@@ -0,0 +1,11 @@
+'use strict'
+var chai = require("chai")
+var expect = chai.expect
+
+describe("Given", () => {
+    describe("When", () => {
+        it("Then true equals true", () => {
+            expect(true).to.eql(true)
+        })
+    })
+})
